c10d16c657efb97bbc3a6ee9f6a340d533bd7166
ummimohamed <ummimohamed@b2allsolution.com>
2019-10-01 12:47:22 +0800

    ngFor
    ngFor directive

--- src/app/test/test/test.component.ts
+++ src/app/test/test/test.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-   public name: string;
+  // public name: string;
   // public myId = 'testId';
   // public successClass = 'text-success';
-  // public greeting: string;
+ public greeting: string;
+ public color = 'blue';
+ public colors = ['red', 'blue', 'green', 'yellow'];
   // public hasError = true;
 
   constructor() { }
@@ -21,10 +23,15 @@ export class TestComponent implements OnInit {
   // function called by front end (html) when button clicked
 // onClick() {
 //   this.greeting = 'Welcome User';
+//   console.log(this.greeting);
 // }
 
 // logMessage(value){
 //   console.log(value);
+// }
+
+// Save() {
+//   console.log(this.name);
 // }
 
 }
