bb2f79fe6c557bd2cb96e3a92f9299a6e00a3a37
ummimohamed <ummimohamed@b2allsolution.com>
2019-10-04 09:21:15 +0800

    pipes
    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test/test.component';
+import { EmployeeComponent } from './test/employee/employee.component';
+import { EmployeeListComponent } from './test/employee/employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './test/employee/employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent,
   ],
   imports: [
     BrowserModule,
