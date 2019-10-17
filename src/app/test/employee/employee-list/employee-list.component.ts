1480235ef8d1201be841570e3e98d6ea7808d43a
UmmiMohamed <55869912+UmmiMohamed@users.noreply.github.com>
2019-10-17 11:35:37 +0800

    adding switch case

--- src/app/test/employee/employee-list/employee-list.component.ts
+++ src/app/test/employee/employee-list/employee-list.component.ts
@@ -10,12 +10,14 @@ export class EmployeeListComponent implements OnInit {
 
   public employees = [];
   public errorMsg: any;
-  // public employees = [
+  // public employee = [
   //   {id: 1, name: 'Andrew', age: '30'},
   //   {id: 2, name: 'Brandon', age: '25'},
   //   {id: 3, name: 'Christina', age: '26'},
   //   {id: 4, name: 'Elena', age: '28'},
   // ];
+
+  public firstEmployee = 'Andrew';
 
   constructor(private employeeService: EmployeeService) { }
 
